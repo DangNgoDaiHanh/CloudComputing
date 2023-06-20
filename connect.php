196e4166ef2726b6fba175d80c7bda5c0870e903
DangNgoDaiHanh <hanhdndgcc210154@fpt.edu.vn>
2023-06-20 15:31:17 +0700

    update asm2

--- connect.php
+++ connect.php
@@ -8,9 +8,9 @@ class Connect{
     public function __construct()
     {
         $this->server ="co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        $this->user ="d17ls0cviegakvap";
-        $this->password ="o6slew7gfo23njvc";
-        $this->dbName="mjgfmppulqpqeds8";
+        $this->user ="h1ocwpaefslspemh";
+        $this->password ="dh4jxlk0kx13q7l2";
+        $this->dbName="grbm8739q7hk2cb3";
     }
 
     //Option 1: use mysqli
